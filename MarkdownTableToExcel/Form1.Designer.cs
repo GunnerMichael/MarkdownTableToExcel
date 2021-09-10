21e0079c1f2caf91d0c28d3277bff71f42696ee0
Michael Butler <Michael.Butler@skipton.co.uk>
2021-09-10 12:03:44 +0100

    first commit

---
+++ MarkdownTableToExcel/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MarkdownTableToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.markdownBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.toExcelCommand = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.markdownBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.888889F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // markdownBox
+            // 
+            this.markdownBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.markdownBox.Location = new System.Drawing.Point(3, 3);
+            this.markdownBox.Multiline = true;
+            this.markdownBox.Name = "markdownBox";
+            this.markdownBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.markdownBox.Size = new System.Drawing.Size(794, 404);
+            this.markdownBox.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.toExcelCommand);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 413);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(300, 34);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // toExcelCommand
+            // 
+            this.toExcelCommand.Location = new System.Drawing.Point(3, 3);
+            this.toExcelCommand.Name = "toExcelCommand";
+            this.toExcelCommand.Size = new System.Drawing.Size(112, 34);
+            this.toExcelCommand.TabIndex = 0;
+            this.toExcelCommand.Text = "To Excel";
+            this.toExcelCommand.UseVisualStyleBackColor = true;
+            this.toExcelCommand.Click += new System.EventHandler(this.toExcelCommand_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox markdownBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button toExcelCommand;
+    }
+}
+
---
+++ MarkdownTableToExcel/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MarkdownTableToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toExcelCommand_Click(object sender, EventArgs e)
+        {
+            string text = markdownBox.Text;
+
+            string[] lines = text.Split("\r\n");
+
+            bool foundHeader = false;
+
+            List<string> columns = new List<string>();
+
+            List<List<string>> data = new List<List<string>>();
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("|-"))
+                {
+                    continue;
+                }
+
+                if (line.StartsWith("|"))
+                {
+                    if (foundHeader == false)
+                    {
+                        string[] cols = line.Substring(line.IndexOf("|") + 1).Split("|");
+
+                        foreach (var item in cols)
+                        {
+                            if (string.IsNullOrEmpty(item) == false)
+                            {
+                                columns.Add(item);
+                            }
+                        }
+
+                        foundHeader = true;
+
+                    }
+                    else
+                    {
+                        string[] cols = line.Substring(line.IndexOf("|") + 1).Split("|");
+
+                        List<string> row = new List<string>();
+
+                        foreach (var item in cols)
+                        {
+                            var itemText = item.Trim();
+
+                            if (string.IsNullOrEmpty(itemText) == false && itemText.Contains("----") == false)
+                            {
+                                row.Add(itemText);
+                            }
+                        }
+
+                        data.Add(row);
+                    }
+                }
+            }
+
+
+
+
+            var excelApp = new Excel.Application();
+            //Make the object visible.
+            excelApp.Visible = true;
+
+            // Create a new, empty workbook and add it to the collection returned
+            // by property Workbooks. The new workbook becomes the active workbook.
+            // Add has an optional parameter for specifying a praticular template.
+            // Because no argument is sent in this example, Add creates a new workbook.
+            excelApp.Workbooks.Add();
+
+            // This example uses a single workSheet. The explicit type casting is
+            // removed in a later procedure.
+            Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            char cell = 'A';
+
+            foreach (var item in columns)
+            {
+                workSheet.Cells[1, $"{cell}"] = item;
+                cell++;
+            }
+
+            //workSheet.Cells[1, "A"] = "ID Number";
+            //workSheet.Cells[1, "B"] = "Current Balance";
+
+            var rowCount = 2;
+
+            foreach(var rowData in data)
+            {
+                cell = 'A';
+                foreach (var col in rowData)
+                {
+                    workSheet.Cells[rowCount, $"{cell}"] = col;
+                    cell++;
+                }
+
+                rowCount++;
+            }
+
+            //foreach (var acct in accounts)
+            //{
+            //    row++;
+            //    workSheet.Cells[row, "A"] = acct.ID;
+            //    workSheet.Cells[row, "B"] = acct.Balance;
+            //}
+
+            for (int i = 1; i <= columns.Count; i++)
+            {
+                workSheet.Columns[i].AutoFit();
+            }
+            //workSheet.Columns[2].AutoFit();
+
+            //// Call to AutoFormat in Visual C#. This statement replaces the
+            //// two calls to AutoFit.
+            //workSheet.Range["A1", "B3"].AutoFormat(
+            //    Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic2);
+
+            //// Put the spreadsheet contents on the clipboard. The Copy method has one
+            //// optional parameter for specifying a destination. Because no argument
+            //// is sent, the destination is the Clipboard.
+            //workSheet.Range["A1:B3"].Copy();
+        }
+    }
+}
